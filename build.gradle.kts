02980c0b7dc547350af67139ca8f6a7053fd7ad7
Andre Raack <andre.raack@porsche-engineering.de>
2025-07-14 14:11:21 +0200

    Update on Version due to missing migration

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,7 +17,7 @@ plugins {
 }
 
 group = "de.pes.MetaBNF"
-version = "2024.1.5"
+version = "2024.1.6"
 description = "MetaBNF provides MPS-based languages for working with Backus-Naur Form (BNF). It enables users to define, edit, and analyze BNF grammars directly in JetBrains MPS, facilitating language design, parsing, and formal grammar processing."
 
 val projectName = "MetaBNF"
